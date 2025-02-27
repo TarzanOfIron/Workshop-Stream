3a25469f93aa3055172922df3b959c61247a4989
Boti <medgyesi.me@icloud.com>
2025-02-27 17:28:21 +0100

    Did most of the tests

--- src/test/java/se/lexicon/vxo/service/StreamExercise.java
+++ src/test/java/se/lexicon/vxo/service/StreamExercise.java
@@ -7,13 +7,10 @@ import se.lexicon.vxo.model.PersonDto;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.Year;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,14 +35,11 @@ public class StreamExercise {
 
     /**
      * Turning people into a Stream count all members
-     * 
      */
     @Test
     public void task2() {
-        long amount = 0;
-
+        long amount = people.stream().count();
         // todo: write your code here
-
         assertEquals(10000, amount);
     }
 
@@ -54,7 +48,7 @@ public class StreamExercise {
      */
     @Test
     public void task3() {
-        long amount = 0;
+        long amount = people.stream().filter(person -> person.getLastName().equals("Andersson")).count();
         int expected = 90;
 
         // todo: write your code here
@@ -68,7 +62,7 @@ public class StreamExercise {
     @Test
     public void task4() {
         int expectedSize = 4988;
-        List<Person> females = null;
+        List<Person> females = people.stream().filter(person -> person.getGender().equals(Gender.FEMALE)).collect(Collectors.toList());
 
         // todo: write your code here
 
@@ -82,7 +76,7 @@ public class StreamExercise {
     @Test
     public void task5() {
         int expectedSize = 8882;
-        Set<LocalDate> dates = null;
+        Set<LocalDate> dates = new TreeSet<>(people.stream().map(Person::getDateOfBirth).collect(Collectors.toSet()));
 
 
         // todo: write your code here
@@ -100,7 +94,7 @@ public class StreamExercise {
     public void task6() {
         int expectedLength = 3;
 
-        Person[] result = null;
+        Person[] result = people.stream().filter(person -> person.getFirstName().equals("Erik")).toArray(Person[]::new);
 
         // todo: write your code here
 
@@ -116,7 +110,8 @@ public class StreamExercise {
     public void task7() {
         Person expected = new Person(5436, "Tea", "Håkansson", LocalDate.parse("1968-01-25"), Gender.FEMALE);
 
-        Optional<Person> optional = null;
+        Optional<Person> optional = people.stream().filter(person -> person.getPersonId() == 5436).findFirst();
+
 
         // todo: write your code here
 
@@ -127,13 +122,13 @@ public class StreamExercise {
     }
 
     /**
-     * Using min() define a comparator that extracts the oldest person i the list as an Optional
+     * Using min() define a comparator that extracts the oldest person in the list as an Optional
      */
     @Test
     public void task8() {
         LocalDate expectedBirthDate = LocalDate.parse("1910-01-02");
 
-        Optional<Person> optional = null;
+        Optional<Person> optional = people.stream().min(Comparator.comparing(Person::getDateOfBirth));
 
         // todo: write your code here
 
@@ -149,7 +144,7 @@ public class StreamExercise {
         int expectedSize = 892;
         LocalDate date = LocalDate.parse("1920-01-01");
 
-        List<PersonDto> dtoList = null;
+        List<PersonDto> dtoList = people.stream().filter(person -> person.getDateOfBirth().isBefore(LocalDate.parse("1920-01-01"))).map(person -> new PersonDto(person.getPersonId(), person.getFirstName() + person.getLastName())).collect(Collectors.toList());
 
         // todo: write your code here
 
@@ -163,12 +158,14 @@ public class StreamExercise {
      * then take the birthdate and build a string from data that the date contains then
      * return the string.
      */
+
     @Test
     public void task10() {
         String expected = "WEDNESDAY 19 DECEMBER 2012";
         int personId = 5914;
 
-        Optional<String> optional = null;
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy");
+        Optional<String> optional = people.stream().filter(person -> person.getPersonId() == personId).findFirst().map(person -> person.getDateOfBirth().format(dateTimeFormatter).toUpperCase());
 
         // todo: write your code here
 
@@ -186,8 +183,11 @@ public class StreamExercise {
     public void task11() {
         double expected = 54.42;
         double averageAge = 0;
-
         // todo: write your code here
+        ToIntFunction<Person> personToAge = person -> Period.between( person.getDateOfBirth(),LocalDate.now()).getYears();
+
+        averageAge = people.stream().mapToInt(personToAge).average().getAsDouble();
+        //averageAge = people.stream().mapToInt(person -> Period.between())
 
         assertTrue(averageAge > 0);
         assertEquals(expected, averageAge, .01);
@@ -196,11 +196,15 @@ public class StreamExercise {
     /**
      * Extract from people a sorted string array of all firstNames that are palindromes. No duplicates
      */
+    boolean isPalindrome(String string) {
+        String backWords = new StringBuilder(string).reverse().toString();
+        return string.equalsIgnoreCase(backWords);
+    }
     @Test
     public void task12() {
         String[] expected = {"Ada", "Ana", "Anna", "Ava", "Aya", "Bob", "Ebbe", "Efe", "Eje", "Elle", "Hannah", "Maram", "Natan", "Otto"};
 
-        String[] result = null;
+        String[] result = people.stream().map(Person::getFirstName).collect(Collectors.toSet()).stream().filter(s -> isPalindrome(s)).sorted().toArray(String[]::new);
 
         // todo: write your code here
 
